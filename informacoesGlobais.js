3ebdc53331b44f36e729542bf65a20677c80cd1a
neuburgg <166568184+neuburgg@users.noreply.github.com>
2024-10-15 21:00:43 -0300

    const

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,3 +1,5 @@
 const url = 'https://github.com/guilhermeonrails/api/commit/c303b58aa062c92e093e41641ab4d60347de5f2c'
 
-console.log(url);
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+}
